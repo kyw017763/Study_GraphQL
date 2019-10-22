76cdc27daa3e78eab6b3b6b670d422f835d43f74
Yewon Kim <37951612+kyw017763@users.noreply.github.com>
2019-10-22 23:40:10 +0900

    Finish with People Tutorial

---
+++ graphql/dbPerson.js
@@ -0,0 +1,14 @@
+export const people = [
+    {  
+        id: "0",
+        name: "Woni",
+        age: 18,
+        gender: "female"
+    },
+    { 
+        id: "1",   
+        name: "Jini",
+        age: 20,
+        gender: "female"
+    }
+];
--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,21 +1,13 @@
-const people = [
-    {  
-        id: "0",
-        name: "Woni",
-        age: 18,
-        gender: "female"
-    },
-    { 
-        id: "1",   
-        name: "Jini",
-        age: 20,
-        gender: "female"
-    }
-];
+import { people } from "./dbPerson"
 
 const resolvers = {
     Query: {
-        people: () => people
+        people: () => people,
+        person: (_, { id }) => { 
+            console.log(id);
+            // {id} 부분을 args로 교체하면 args.id 형태로 써야해서 번거로움
+            return people.filter(person =>  person.id === String(id))[0];
+        }
     }
 };
 
